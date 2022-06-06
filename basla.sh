740f316c81733f02f77d9643a458592551bf74cb
erdem149 <149erdem@gmail.com>
2022-06-07 01:03:17 +0300

    1.2 - Kod düzenlendi ve fonksonlara ayrıldı

--- basla.sh
+++ basla.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 # Colors
+
 purple=$'\033[1;34m'
 reset=$'\033[0;39m'
 red=$'\033[0;31m'
 green=$'\033[0;32m'
 echo "hoşgeldiniz"
-# Commands
-gcc sec.c -o sec
-mv sec ./src
-cd ./sorular
-read -p "${purple}pop'a başlamak istermisiniz ?[${green}y${purple}/${red}N${purple}]${reset}" input
-echo ""
-if [ -n "$input" ] && [ "$input" = "y" ]; then
-	for((i = 0; i <= 3 ; i++)) do
+# function
+function atama()
+{
 	SORULAR=$(ls)
 	SORUSAYISI=$(ls -l | wc -l)-1
 	RSAYI=$((RANDOM%SORUSAYISI))
@@ -23,20 +19,37 @@ if [ -n "$input" ] && [ "$input" = "y" ]; then
 	cev=$(../src/cevap)
 	mv $SECILENSORU ../trash
 	echo
-	read  -p "${purple}Çiktiyi yaziniz : ${reset} " input
-	if [ "$input" ] && [ "$input" = "$cev" ]; then
-		echo ${green}[OK]${reset}
-	else
-		echo
-		read -p "${purple}Yanlis tekrar deneyiniz : ${reset} " input
+}
+
+function sor()
+{
+	for((i = 0; i <= 3 ; i++)) do
+		atama
+		read  -p "${purple}Çiktiyi yaziniz : ${reset} " input
 		if [ "$input" ] && [ "$input" = "$cev" ]; then
 			echo ${green}[OK]${reset}
 		else
-			echo ${red}[KO]${reset}
+			echo
+			read -p "${purple}Yanlis tekrar deneyiniz : ${reset} " input
+			if [ "$input" ] && [ "$input" = "$cev" ]; then
+				echo ${green}[OK]${reset}
+			else
+				echo ${red}[KO]${reset}
+			fi
 		fi
-	fi
 	done
+	mv ../trash/* ./
+}
+# Commands
+
+gcc sec.c -o sec
+mv sec ./src
+cd ./sorular
+read -p "${purple}pop'a başlamak istermisiniz ?[${green}y${purple}/${red}N${purple}]${reset}" input
+echo ""
+if [ -n "$input" ] && [ "$input" = "y" ]; then
+	sor
 fi
-mv ../trash/* ./
+
 
 # created by eryilmaz, rtosun, ademirci
